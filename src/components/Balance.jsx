7eaa5d4e065a57df3d43be6dd4dc715b3ea714c5
samirm00 <sammoham.be@gmail.com>
2021-04-16 17:06:45 +0200

    add currency sign

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -5,7 +5,7 @@ const Balance = () => {
   const amount = useSelector((state) => state.banking.amount);
   return (
     <div className="mt-4">
-      <h1>{amount}</h1>
+      <h1>$ {amount}</h1>
     </div>
   );
 };
